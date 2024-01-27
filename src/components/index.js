6077d6eb3c76866cd05a88cc490f4351f0d202b9
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-01-27 13:55:41 +0530

    project completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./footer/Footer";
-export { Header, Footer }
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import Protected from "./AuthLayout";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+export { Header, Footer, Container, Logo, LogoutBtn, Button, PostForm, PostCard, RTE, Select, Signup, Login, Input, Protected }
